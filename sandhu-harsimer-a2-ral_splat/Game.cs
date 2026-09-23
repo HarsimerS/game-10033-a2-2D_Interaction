49157a6440aece619a3e36ab0de8be4927d8d816
Furious <harsimer1111@gmail.com>
2026-09-23 02:31:38 -0400

    add basic TODO list and declared variables

--- sandhu-harsimer-a2-ral_splat/Game.cs
+++ sandhu-harsimer-a2-ral_splat/Game.cs
@@ -11,6 +11,13 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
+        const int GROUND_Y = 220;
+        const int HURT_FRAMES_APPLIED = 20;
+        const int SPLAT_FRAMES_APPLIED = 90;
+        const int HURT_DRAIN = 1;
+        int hurt_timer = 0;
+        int splat_timer = 0;
+        Vector2 center_position = new Vector2(200, 200);
 
 
         /// <summary>
@@ -18,7 +25,10 @@ namespace MohawkGame2D
         /// </summary>
         public void Setup()
         {
-
+            // Basically the premise is that you "poke" Ralsie from Deltarune
+            Window.SetTitle("Poke Ralsie");
+            // The max size the assignement has mentioned
+            Window.SetSize(400, 400);
         }
 
         /// <summary>
@@ -26,7 +36,20 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            // Since pixels drawn don't get auto cleared on their own, we have to manually clear everything at the start of every frame
+            Window.ClearBackground(Color.White);
 
+            // TODO: While idle (no inputs pressed), we draw Ralsie's idle sprite at the center, facing towards the "camera", aka, you
+
+            // TODO_2: If Mouse Left Click is detected within the specified range of Ralsie, `hurt_timer` gets increased by `HURT_FRAMES_APPLIED`
+            // Continue; Then every frame lowers it by `HURT_DRAIN`, so once it reaches 0, Ralsie goes back to idle sprite, otherwise, stays in hurt sprite
+
+            // TODO_3: If Mouse Right Click is held, Ralsie's center position is latched to the mouse for as long as Right Click is held. There should be a latched sprite when held
+            // Continue; If the mouse lets go, Ralsie should start falling with increasing gravity. Upon landing, by reaching `GROUND_Y`, Ralsie will go into splat sprite
+            // Continue; Ralsie will remain in that sprite until the timer `splat_timer` reaches 0, which initially get increased from `SPLAT_FRAMES_APPLIED`
+
+
+            // TODO_4: Ralsie can't be interacted with until `splat_timer` reaches 0
         }
     }
 
